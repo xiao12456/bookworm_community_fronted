47b5509782c166bd946c5b1012498ab0c8683661
wangwu <wangwu228056@gmail.com>
2024-04-02 16:26:09 +0800

    公告板

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="box">🔔 {{ billboard }}</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      billboard: '版本更新'
+    }
+  }
+}
+</script>
