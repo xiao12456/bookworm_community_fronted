9e192284d939ab71c2b0f70f886e399b8316251e
wangwu <wangwu228056@gmail.com>
2024-04-05 22:41:12 +0800

    推广

--- src/views/card/Promotion.vue
+++ src/views/card/Promotion.vue
@@ -4,25 +4,35 @@
       <span>🥂 推广</span>
     </div>
     <div>
-      111111
+      <p v-for="(item, index) in list" :key="item.id" class="block">
+        <a :href="item.link" target="_blank" :title="item.description">{{ item.title }}</a>
+      </p>
     </div>
   </el-card>
 </template>
 
 <script>
+import { getList } from '@/api/promotion'
 
 export default {
   name: 'Promotion',
   data() {
     return {
-      
+      list: []
     }
   },
   created() {
-    
+    this.fetchList()
   },
   methods: {
-    
+    async fetchList() {
+      getList().then(response => {
+        const { data } = response
+        this.list = data;
+      }).catch(() => {
+
+      })
+    },
   }
 }
 </script>
